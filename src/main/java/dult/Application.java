a479421824c680fa2db57e1f94b425779e657a3d
dult-1 <1769140517@qq.com>
2021-10-31 18:53:16 +0800

    eureka测试

---
+++ src/main/java/dult/Application.java
@@ -0,0 +1,17 @@
+package dult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心启动
+ * Created by dult on 2021-10-29.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
