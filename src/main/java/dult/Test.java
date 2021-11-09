8e3cdf1cd2dc0da9caf2120b01881d41f1d1a17f
dult-1 <1769140517@qq.com>
2021-11-09 15:21:02 +0800

    hystrix-dashboard 仪表盘及信号量测试

---
+++ src/main/java/dult/Test.java
@@ -0,0 +1,50 @@
+package dult;
+
+
+
+import org.nutz.http.Http;
+import org.nutz.http.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by dult on 2021-11-9.
+ */
+public class Test {
+    public static final Semaphore SEMAPHORE = new Semaphore(3);
+    //    public static InheritableThreadLocal<Integer> threadLocal = new InheritableThreadLocal<>();
+    public static ExecutorService service = Executors.newFixedThreadPool(5);
+    public static volatile int num=0;
+    public static void main(String[] args) {
+        for (int i = 0; i <30 ; i++) {
+            service.submit(new TestRunnable());
+        }
+        service.shutdown();
+    }
+
+    static class TestRunnable implements Runnable{
+        @Override
+        public void run() {
+            try {
+                SEMAPHORE.acquire();
+                Map<String,Object> map = new HashMap<>();
+                int resultNum = getNum();
+                map.put("id",resultNum);
+                System.out.println("=id==" + resultNum);
+                Response response = Http.get("http://localhost:9000/ribbon-consumer-find",map,5000);
+                System.out.println("===" + response.getContent());
+                SEMAPHORE.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized int getNum(){
+            return num++;
+        }
+    }
+}
